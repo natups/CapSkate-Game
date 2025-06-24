501c4990e86621701ccc4dcd62fac8942ba2c3fb
Natasha Pistarelli <natu123pistarelli@gmail.com>
2025-06-24 13:32:25 -0300

    template limpio para usar

--- main.js
+++ main.js
@@ -1,34 +1,26 @@
 import Game from "./scenes/Game.js";
 
-// Create a new Phaser config object
+// Crear configuración de Phaser
 const config = {
   type: Phaser.AUTO,
-  width: 720,
-  height: 720,
+  width: 320, // Resolución base tipo Sega Genesis
+  height: 240,
+  pixelArt: true, // Para mantener los píxeles nítidos
+  roundPixels: true, // Redondear los píxeles para evitar desenfoque
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
-    min: {
-      width: 800,
-      height: 600,
-    },
-    max: {
-      width: 1600,
-      height: 1200,
-    },
+    width: 320,
+    height: 240,
   },
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: 200 },
-      debug: true,
+      gravity: { y: 980 }, // Gravedad tipo terrestre en píxeles por segundo cuadrado
+      debug: false,
     },
   },
-  // List of scenes to load
-  // Only the first scene will be shown
-  // Remember to import the scene before adding it to the list
   scene: [Game],
 };
 
-// Create a new Phaser game instance
 window.game = new Phaser.Game(config);
