77c8b7f271daa5cdc1b22d43030db8a41f9723bd
Natasha Pistarelli <natu123pistarelli@gmail.com>
2025-06-24 22:05:46 -0300

    menu con parallax funcionando

--- scenes/Game.js
+++ scenes/Game.js
@@ -5,16 +5,27 @@ export default class Game extends Phaser.Scene {
     super("game");
   }
 
-  init() {
-  }
-
   preload() {
+    this.load.image("cielo", "public/assets/cielo.png");
+    this.load.image("nubes", "public/assets/nubes.png");
+    this.load.image("titulo", "public/assets/titulo.png");
   }
 
   create() {
+    // Fondo más lejano (cielo)
+    this.fondoCielo = this.add.tileSprite(0, 0, 320, 240, "cielo").setOrigin(0, 0);
+
+    // Nubes (más cercanas)
+    this.fondoNubes = this.add.tileSprite(0, 0, 320, 240, "nubes").setOrigin(0, 0);
+
+    // Título centrado
+    this.titulo = this.add.image(160, 100, "titulo").setOrigin(0.5, 0.5);
   }
 
   update() {
-
-    }
+    // Movimiento parallax: cuanto más al fondo, más lento
+    this.fondoCielo.tilePositionX += 0.05; // cielo se mueve más lento
+    this.fondoNubes.tilePositionX += 0.2;  // nubes más rápido
   }
+}
+
